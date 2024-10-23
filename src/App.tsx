ff529fc836bc946b020ea2a18063433d2cfaf7d3
binaryhexa <gif.glses@gmail.com>
2024-10-23 11:30:27 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-4xl text-red-600">
+        First Setup for Baso Krijo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
