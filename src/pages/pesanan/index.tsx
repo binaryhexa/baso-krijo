704c3492e19b7cade8f8726518dd6649be2a5428
binaryhexa <gif.glses@gmail.com>
2024-11-07 16:18:33 +0700

    [FEAT]: pesanan tab menu

--- src/pages/pesanan/index.tsx
+++ src/pages/pesanan/index.tsx
@@ -1,7 +1,10 @@
+import PesananTab from "./components/PesananTab";
+
 const Pesanan = () => {
     return (
       <div>
         <h1 className="font-semibold text-2xl">Daftar Pesanan</h1>
+        <PesananTab />
       </div>
     );
   };
