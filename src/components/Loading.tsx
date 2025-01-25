a5fe324814f19695efa98ca1c5cd3bcfe20fd5eb
binaryhexa <gif.glses@gmail.com>
2025-01-25 23:12:58 +0700

    feat: loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,12 @@
 const Loading = () => {
   return (
-    <div className="flex text-center justify-center gap-3">
-      <div className="border-primary100 rounded-full w-6 h-6 border-2 border-r-primary50 animate-spin"></div>
-      <div className="text-center">Mohon Tunggu...</div>
+    <div className="flex h-screen text-center m-auto justify-center gap-3 bg-primary50">
+      <div className="m-auto">
+        <div className="border-primary90 m-auto rounded-full w-10 h-10 border-[6px] mb-2 border-r-primary60 animate-spin"></div>
+        <div className="text-center text-white text-xl font-medium">
+          Mohon Tunggu...
+        </div>
+      </div>
     </div>
   );
 };
